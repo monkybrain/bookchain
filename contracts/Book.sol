9da2a501e791ad8114b8901f1205690af8fe1861
Philip Prophet <philip.prophet@gmail.com>
2017-10-22 12:35:43 +0200

    Simple contract (written in Remix)

--- contracts/Book.sol
+++ contracts/Book.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.18;
 
 contract Book {
 
@@ -9,16 +9,33 @@ contract Book {
 
   event Borrowed(address borrower);
 
-  function Book(string _isbn, uint _price) {
+  function Book(string _isbn, uint _price) public {
     contributor = msg.sender;
-    borrower = msg.sender;
     isbn = _isbn;
-    price = _price;
+    price = _price * (10**18);
   }
 
-  function borrow() payable {
+  function borrow() payable public {
+
+    // Set sender as new borrower
     borrower = msg.sender;
+
+    // Cap donations to price (return exceeding funds to borrower)
+    var remaining = price - (this.balance - msg.value);
+    if (msg.value > remaining) {
+        var diff = msg.value - remaining;
+        if (!msg.sender.send(diff)) revert();
+    }
+
     Borrowed(borrower);
+  }
+
+  function getBalance() public view returns(uint) {
+     return this.balance;
+  }
+
+  function getRemaining() public view returns(uint) {
+      return price - this.balance;
   }
 
 }
